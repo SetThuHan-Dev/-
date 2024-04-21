a3d13a59d490516291e9941f9fb72e5e5f9651e1
Shin <113447169+SetThuHan-Dev@users.noreply.github.com>
2024-04-22 03:43:08 +0630

    update js

--- scripts/button_nav_proj.js
+++ scripts/button_nav_proj.js
@@ -31,7 +31,7 @@ let projTextArea = document.getElementById("project_data_area");
 for(let i = 0; i < ProjectNames.length; ++i)
 {
     ProjectText.push("");
-    fetch("./Data/pages/projects/" + ProjectNames[i] + ".html?v=2")
+    fetch("./Data/pages/projects/" + ProjectNames[i] + ".html")
     .then( r => r.text() )
     .then( t => ProjectText[i] = t )
 }
